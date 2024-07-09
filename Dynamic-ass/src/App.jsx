53764d21d8a635f2c0866749cf88a6848f376515
Roselineb <roseline12004@gmail.com>
2024-07-09 14:04:46 +0530

    first commit

---
+++ Dynamic-ass/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { FaRegEye } from "react-icons/fa6";
+import Headercomponent from './assets/Components/Headercomponent.jsx'
+function App() {
+  
+
+  return (
+    <>
+    <div className='box'><FaRegEye  className='icon'/>
+    <Headercomponent name="ROSELINE" regno="212221220046"/>
+    </div>
+   
+
+
+    </>
+  )
+}
+
+export default App
